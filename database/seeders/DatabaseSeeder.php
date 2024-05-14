15d034470937d75a8fe52ce2bcaa8b6fa738d777
Diego Schmitt <diego_asms@hotmail.com>
2024-05-13 22:57:52 -0300

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Task;
+use App\Models\Category;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(4)->create();
+        Category::factory(30)->create();
+        Task::factory(100)->create();
+
+    }
+}
