df956306c1a3c337f947c13a3609c01cb116490c
Diego Schmitt <diego_asms@hotmail.com>
2024-05-16 22:15:29 -0300

    criao de rotase controllers para navegao

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Authcontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/login', function(){
-    return view('login');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
+Route::get('/task', [TaskController::class, 'index'])->name('task.view');
+Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
